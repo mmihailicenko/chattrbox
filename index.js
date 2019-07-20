d75bc2bcc3ce2891711c10e474e8bdcc45f0be18
mmihailicenko <mihails.mihailicenko@zopa.com>
2019-07-20 15:01:51 +0300

    Chapter 15

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 var http = require('http');
+var fs = require('fs');
+var extract = require('./extract');
+
+var handleError = function (err, res) {
+    res.writeHead(404);
+    res.end();
+};
 
 var server = http.createServer(function (req, res) {
     console.log('Responding to a request.');
-    res.end('<h1>Hello, World</h1>h1>')
+
+    var filePath = extract(req.url);
+    fs.readFile(filePath, function (err, data) {
+        if (err) {
+            handleError(err, res);
+            return;
+        } else {
+            res.end(data);
+        }
+    });
 });
 
 server.listen(3000);
